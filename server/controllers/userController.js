474eda8ce16f9391364f4fe81aa04ef4db14e9e5
Selim Mahmud <selim.mahmud80@yahoo.com.au>
2018-04-30 21:07:28 +0930

    user created

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,15 +1,39 @@
 const user = require('../models').user;
 
 module.exports = {
+
     list(req, res) {
         return user
             .findAll({})
             .then(users => res.status(200).send(users))
             .catch(error => res.status(400).send(error));
     },
-    create(req, res) {
-        //
+
+    create(req, res, next){
+
+        const email = req.body.email;
+        const password = req.body.password;
+
+        user.findOne({where: {email: email}})
+            .then(existingUser => {
+                if (existingUser) {
+                    return res.status(422).send({
+                        error: 'Email is in use',
+                    });
+                }
+
+                user.create({email, password}, {isNewRecord: true})
+                    .then(createdUser => {
+                        return res.status(200).send({
+                            data: createdUser,
+                        });
+                    })
+                    .catch(error => res.status(400).send(error));
+
+            })
+            .catch(error => res.status(400).send(error));
     },
+
     retrieve(req, res) {
         return user
             .findById(req.params.userId, {})
