67eb0a5a0b3718d41915d0e31477872650442296
Selim Mahmud <selim.mahmud80@yahoo.com.au>
2018-05-03 21:51:44 +0930

    dotenv package has been used

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,10 @@
+require('dotenv').config();
+
 module.exports = {
-    appSecret: 'dcksncsjdcjdxcdfpoie32m'
+    appSecret: process.env.APP_SECRET,
+    dbUsername: process.env.DB_USERNAME,
+    dbPassword: process.env.DB_PASSWORD,
+    dbDatabase: process.env.DB_DATABASE,
+    dbHost: process.env.DB_HOST,
+    dbDialect: process.env.DB_DIALECT,
 }
--- server/models/index.js
+++ server/models/index.js
@@ -2,39 +2,34 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(module.filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(`${__dirname}/../config/config.json`)[env];
+const config = require('../config/config');
 const db = {};
 
-const Op = Sequelize.Op
+const Op = Sequelize.Op;
 let sequelize;
-if (config.use_env_variable) {
-    sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else {
-    sequelize = new Sequelize(
-        config.database,
-        config.username,
-        config.password,
-        {
-            host: config.host,
-            dialect: config.dialect,
-            logging: false,
-            freezeTableName: true,
-            operatorsAliases: {
-                $and: Op.and,
-                $or: Op.or,
-                $eq: Op.eq,
-                $gt: Op.gt,
-                $lt: Op.lt,
-                $lte: Op.lte,
-                $like: Op.like
-            }
-        }
-    );
-}
 
-fs
-    .readdirSync(__dirname)
+sequelize = new Sequelize(
+    config.dbDatabase,
+    config.dbUsername,
+    config.dbPassword,
+    {
+        host: config.dbHost,
+        dialect: config.dbDialect,
+        logging: false,
+        freezeTableName: true,
+        operatorsAliases: {
+            $and: Op.and,
+            $or: Op.or,
+            $eq: Op.eq,
+            $gt: Op.gt,
+            $lt: Op.lt,
+            $lte: Op.lte,
+            $like: Op.like
+        }
+    }
+);
+
+fs.readdirSync(__dirname)
     .filter(file =>
         (file.indexOf('.') !== 0) &&
         (file !== basename) &&
